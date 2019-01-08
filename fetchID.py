aba605803509e7a55d40d2ea6e66934f084fd83c
Ali60351 <ali60351@gmail.com>
2019-01-08 21:58:16 +0500

    Optimized Code

--- fetchID.py
+++ fetchID.py
@@ -12,28 +12,21 @@ user_ids = []
 next_flag = True
 
 while next_flag:
-    json_result = api.user_following(self_ID, restrict='public', offset=i, req_auth=True)
+    json_result = api.user_following(self_ID, offset=i)
 
     for users in json_result['user_previews']:
         user_ids.append(users['user']['id'])
-        pass
 
     if json_result.get('next_url'):
         next_flag = True
-        pass
     else:
         next_flag = False
-        pass
-    pass
 
-    print('Fetched ' + user_ids.__len__().__str__() + ' user IDs')
+    print(f'Fetched {len(user_ids)} user IDs')
     i += 30
-    pass
 
 with open('List.txt', 'w') as outfile:
     for ids in user_ids:
-        outfile.write(ids.__str__() + "\n")
-        pass
-    pass
+        outfile.write(str(ids) +  "\n")
 
 print('Fetching complete')
